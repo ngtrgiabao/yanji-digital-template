388c27acae7104e4aff22e2f9987db910dea6eb9
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-06-05 18:36:42 +0700

    add inf slider

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from("nav", { duration: 1.2, ease: "power2.out", y: -100 });
 
 const tl = gsap.timeline({
@@ -6,17 +8,62 @@ const tl = gsap.timeline({
     },
 });
 
-tl.from(".slogan__action-btn", {
+// hero
+
+tl.from(".slogan__sub-slogan", {
+    y: 100,
+    ease: "power2.out",
     opacity: 0,
-})
-    .from(".slogan__logo", {
-        y: 100,
-        ease: "power2.out",
-        opacity: 0,
-    })
-    .from(".slogan__sub-slogan", {
-        y: 100,
-        ease: "power2.out",
-        opacity: 0,
-        stagger: 0.2,
-    });
+    stagger: 0.2,
+}).from(".slogan__action-btn", {
+    opacity: 0,
+});
+
+gsap.to(".slogan", {
+    scrollTrigger: {
+        trigger: ".slogan",
+        start: "60% 20%",
+        end: "60% 20%",
+        toggleActions: "play none reverse none",
+        scrub: 5,
+    },
+    x: 2000,
+});
+
+gsap.to(".container__rainbow-card", {
+    scrollTrigger: {
+        trigger: ".container__rainbow-card",
+        start: "top bottom",
+        end: "top center",
+        toggleActions: "play none none none",
+    },
+
+    opacity: 1,
+    scale: 1,
+});
+
+gsap.to(".banner__container-desc", {
+    yPercent: -40,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".banner__container",
+        start: "top center",
+        end: "+=300 +=300",
+        toggleActions: "play none none reverse",
+        scrub: 2,
+    },
+});
+
+gsap.to(".flip-card", {
+    scrollTrigger: {
+        trigger: ".flip",
+        start: "top center",
+        end: "center center",
+        toggleActions: "play none none none",
+    },
+
+    opacity: 1,
+    duration: 1.2,
+    ease: "power1.out",
+    stagger: 0.2, // Adjust the stagger delay to control the interval between flip cards
+});
